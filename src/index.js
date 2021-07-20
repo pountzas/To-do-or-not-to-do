0b1aaad1fede104becb58592795bddf9449d6c67
pountzas <nikos@pountzas.gr>
2021-07-20 19:52:55 +0300

    add JS list array and add list items to html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 import './style.css';
+
+const list = [
+  {
+    description: 'test',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'test 2',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'test 3',
+    completed: false,
+    index: 2,
+  },
+];
+
+function listShow() {
+  const sortList = list.sort((a, b) => a.index - b.index);
+
+  for (let i = 0; i < sortList.length; i += 1) {
+    document.getElementById('list').insertAdjacentHTML('beforeend', `
+    <div class="listItem">
+    <div>
+      <input class="checkbox" id="item-${i}" type="checkbox">  
+      <p>${sortList[i].description}</p>
+    </div>
+    <button type="button">
+      <img src="https://static.thenounproject.com/png/2854151-200.png" width="12" alt="Dots">
+    </button>
+  </div>`);
+  }
+}
+
+window.onload = listShow();
