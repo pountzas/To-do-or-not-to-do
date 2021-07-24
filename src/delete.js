08489570e8ad4fcd2d0e30379a45a07945e82691
pountzas <nikos@pountzas.gr>
2021-07-24 05:01:36 +0300

    2 modules 1 for clear tasks and 1 for delete individual

--- src/delete.js
+++ src/delete.js
@@ -1,40 +1,31 @@
+/* eslint-disable no-restricted-globals */
 /* eslint-disable no-unused-vars */
 import updateStorage from './storage';
 // eslint-disable-next-line import/no-cycle
 import sortList from './index';
 
 function onClickClear() {
-  // let val = this.value;
-  // console.log(sortList.completed);
-  for (let i = 0; i < sortList.length; i += 1) {
-    // console.log(sortList[i].completed);
-    if (sortList[i].completed === 'true' || sortList[i].completed === true) {
+  for (let i = sortList.length - 1; i >= 0; i -= 1) {
+    if (sortList[i].completed === 'true') {
       sortList.splice(i, 1);
-      console.log('kill task }');
-
-      localStorage.clear();
-      for (let i = 0; i < sortList.length; i += 1) {
-        sortList[i].index = i;
-        // console.log(updateStorage);
-        updateStorage(sortList[i]);
-      }
     }
   }
+  localStorage.clear();
+  for (let i = 0; i < sortList.length; i += 1) {
+    sortList[i].index = i;
+    updateStorage(sortList[i]);
+  }
   location.reload();
-  console.log(sortList);
-  // location.reload();
-  // sortList.push(obj);
-  // updateStorage(obj);
-  // taskInput.value = '';
-  // eslint-disable-next-line no-restricted-globals
-  // 
 }
 
-function onClickDelete() {
-  console.log('delete task');
+export default function onClickDelete(index) {
+  sortList.splice(index, 1);
+  localStorage.clear();
+  for (let i = 0; i < sortList.length; i += 1) {
+    sortList[i].index = i;
+    updateStorage(sortList[i]);
+  }
+  location.reload();
 }
 
-const clearTasks = document.querySelector('.clearBtn').addEventListener('click', onClickClear);
-const taskDel = document.querySelector('.clearBtn').addEventListener('click', onClickDelete);
-
-export default { onClickClear, onClickDelete };
+document.querySelector('.clearBtn').addEventListener('click', () => { onClickClear(); });
