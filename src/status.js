83b3233bbf62861b36edd69256e6505534e238a7
pountzas <nikos@pountzas.gr>
2021-07-24 04:59:56 +0300

    import sortlist to update the DOM

--- src/status.js
+++ src/status.js
@@ -1,7 +1,11 @@
+// eslint-disable-next-line import/no-cycle
+import sortList from './index';
+
 export default function loadCheckboxes() {
   const checkboxes = document.querySelectorAll('.checkbox');
   for (let i = 0; i < checkboxes.length; i += 1) {
     checkboxes[i].addEventListener('click', () => {
+      sortList[i].completed = checkboxes[i].checked;
       const status = [];
       for (let j = 0; j < checkboxes.length; j += 1) {
         status.push(checkboxes[j].checked);
