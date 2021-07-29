ba89e39217867ab750c9cde3bdc789fb6c778eed
pountzas <nikos@pountzas.gr>
2021-07-30 01:29:02 +0300

    add test for checkboxes

--- src/status.test.js
+++ src/status.test.js
@@ -1,3 +1,9 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import loadCheckboxes from "./status";
+
 class LocalStorageMock {
   constructor() {
     this.store = {};
@@ -24,3 +30,25 @@ document.body.innerHTML = `<input id="taskInput" value="Something">
     <i class="far fa-calendar-plus"></i>
     <div id="list"></div>
     <i class="clearBtn"></i>`;
+
+describe('Testing function for updating an items completed status.', () => {
+  test('it sould check if completed tasks removed', () => {
+    global.localStorage = new LocalStorageMock();
+    localStorage.setItem('completed', 'true,false,true');
+    localStorage.setItem('index', '0,1,2');
+    localStorage.setItem('description', 'Task 1,Task 2,Task 3');
+
+    const taskInput = document.getElementById('taskInput');
+    let sortList = [];
+    taskInput.addEventListener('click', () => {
+      sortList = loadCheckboxes();
+    });
+
+    taskInput.click();
+    expect(sortList).toBe(loadCheckboxes());
+  });
+
+  test('It sould add a task to sortList', () => {
+    expect(1).toBe(1);
+  });
+});
