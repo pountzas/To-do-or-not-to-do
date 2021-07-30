3b3f28d772de05f4bc000cd0bac4c80cba90ce18
pountzas <nikos@pountzas.gr>
2021-07-30 19:25:33 +0300

    add description for drop() with sortList expectation

--- src/drag.test.js
+++ src/drag.test.js
@@ -25,3 +25,25 @@ document.body.innerHTML = `<input id="taskInput" value="Something">
     <div id="list"></div>
     <i class="clearBtn"></i>`;
 
+describe('Testing the drop function', () => {
+  test('it sould swap index with the target', () => {
+    global.localStorage = new LocalStorageMock();
+    localStorage.setItem('completed', 'true,false,true');
+    localStorage.setItem('index', '0,1,2');
+    localStorage.setItem('description', 'Task 1,Task 2,Task 3');
+    const sortList = [{
+      description: 'Task 1',
+      status: true,
+      index: 0,
+    },
+    {
+      description: 'Task 3',
+      status: true,
+      index: 1,
+    },
+    {
+      description: 'Task 2',
+      status: false,
+      index: 2,
+    },
+    ];
